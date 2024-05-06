6e78b3908e73aea67f7b0677d81692ddef7aac33
v1dusss <vsivanat@student.42heilbronn.de>
2024-05-06 19:22:05 +0200

    inclued minishel header file

---
+++ includes/minishell.h
@@ -0,0 +1,20 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../libft/includes/libft.h"
+# include <errno.h>
+# include <fcntl.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <signal.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/select.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+#endif
