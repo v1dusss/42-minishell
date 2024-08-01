86180ddae4effe6af9ea352749384b4011081e2c
v1dusss <vsivanat@student.42heilbronn.de>
2024-08-01 15:31:16 +0200

    fixed ft_strcmp

--- libft/src/string/ft_strcmp.c
+++ libft/src/string/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: vsivanat <vsivanat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 12:03:32 by vsivanat          #+#    #+#             */
-/*   Updated: 2024/05/03 12:04:08 by vsivanat         ###   ########.fr       */
+/*   Updated: 2024/08/01 15:29:22 by vsivanat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strcmp(const char *s1, const char *s2)
 	size_t	i;
 
 	i = 0;
-	while ((unsigned char)s1[i] == (unsigned char)s2[i] && !s1 && !s2)
+	while ((unsigned char)s1[i] == (unsigned char)s2[i] && s1[i] && s2[i])
 		i++;
 	if (s1[i] == '\0' && s2[i] == '\0')
 		return (0);
